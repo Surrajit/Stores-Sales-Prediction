2c7a52b287a36b28e1c87fe38016bef9219225a6
Surrajit <152585200+Surrajit@users.noreply.github.com>
2024-09-30 12:24:13 +0530

    Updated Notebook

---
+++ demo.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+
+'''
+if __name__=="__main__":
+    logging.info("Logging has Statrted")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by Zero")
+        raise CustomException(e,sys)
+    
+    '''
