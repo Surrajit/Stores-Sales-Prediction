475f02ff91ed31641da5a77eb192da62bb7dbca5
Surrajit <152585200+Surrajit@users.noreply.github.com>
2024-10-03 11:26:49 +0530

    Data Ingestion

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
+
+'''
 import os
 import sys
 from src.logger import logging
 from src.exception import CustomException
 
 
-'''
+
 if __name__=="__main__":
     logging.info("Logging has Statrted")
 
@@ -14,4 +16,15 @@ if __name__=="__main__":
         logging.info("Division by Zero")
         raise CustomException(e,sys)
     
-    '''
+    
+
+mongo_db_url = os.getenv("MONGODB_URL")
+print(mongo_db_url)
+
+
+'''
+from src.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
+
